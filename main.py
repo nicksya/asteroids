fe7ffe061c3044bf4da06bf5fb41a4e9ff96b5b6
Mykyta Khomenko <mykyta.khomenko@gmail.com>
2025-12-07 21:07:08 +0100

    Spaceship is now moving

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
-    #pygame.init()
-    #screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     #pygame.display.set_mode() 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -18,7 +24,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        #player.update(dt)
+        #player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        #print(f"dt = {dt}")
 
 if __name__ == "__main__":
     main()
