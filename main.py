1ec8a09e2aa617d2b11279f204875620c801ec38
Mykyta Khomenko <mykyta.khomenko@gmail.com>
2026-01-04 08:41:37 +0100

    Adding asteroids p.3

--- main.py
+++ main.py
@@ -11,22 +11,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    #pygame.display.set_mode() 
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroid = Asteroid(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, ASTEROID_MAX_RADIUS)
     asteroid_field = AsteroidField()
+
+    dt = 0
 
     while True:
         log_state()
@@ -34,14 +33,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        updatable.update(dt)
+
         screen.fill("black")
-        for thing in updatable:
-            thing.update(dt)
+
+        #for thing in updatable:
+        #    thing.update(dt)
+
         for thing in drawable:
             thing.draw(screen)
 
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
